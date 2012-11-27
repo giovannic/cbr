584ebddabc4ba285c1af35d816a76b3bce21b70d
Adam Fiksen <af1410@ic.ac.uk>
2012-11-27 15:42:58 +0000

    fixed similarity2

--- compute_similarity2.m
+++ compute_similarity2.m
@@ -1,11 +1,11 @@
 function [ similarity ] = compute_similarity2( case1, case2 )
 %COMPUTE_SIMILARITY2 computes similarity based on euclidean distance
 %   Detailed explanation goes here
-attributes1 = zeroes(45,1);
+attributes1 = zeros(1,45);
 for i = 1 : length(case1.au_vector)
     attributes1(case1.au_vector(i)) = 1;
 end
-attributes2 = zeroes(45,1);
+attributes2 = zeros(1,45);
 for i = 1 : length(case2.au_vector)
     attributes2(case2.au_vector(i)) = 1;
 end
