5e3ea2938b31e44da74d74ac846c03a3a1f992c7
Adam Fiksen <af1410@ic.ac.uk>
2012-11-27 13:42:26 +0000

    can create a case

---
+++ create_case.m
@@ -0,0 +1,9 @@
+function [ cbr_case ] = create_case( au_vector, label )
+%CREATE_CASE Summary of this function goes here
+%   Detailed explanation goes here
+cbr_case.au_vector = au_vector;
+cbr_case.solution = label;
+cbr_case.typicality = 1;
+
+end
+
