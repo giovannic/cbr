5395c56b6297c9f5ee163ce02c8ca8581c929b36
Adam Fiksen <af1410@ic.ac.uk>
2012-11-28 13:08:57 +0000

    seems to be more accurate looking for the least typical

--- retrieve_simple.m
+++ retrieve_simple.m
@@ -6,8 +6,8 @@ similarity = 0;
 for i = 1 : length(cbr)
     this_similarity = compute_similarity_distance(cbr(i), new_case);
     if this_similarity > similarity
-        matched_cases = cbr(i)
-        similarity = this_similarity
+        matched_cases = cbr(i);
+        similarity = this_similarity;
     elseif this_similarity == similarity
         matched_cases = [ matched_cases, cbr ];
     end
@@ -20,9 +20,9 @@ end
 end
 
 function [ typical_case ] = get_typical_case( cases )
-typicality = 0;
+typicality = Inf;
     for i = 1 : length(cases)
-        if cases(i).typicality > typicality
+        if cases(i).typicality < typicality
             typical_case = cases(i);
             typicality = cases(i).typicality;
         end
