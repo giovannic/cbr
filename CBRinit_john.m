1ee460c01c27863ff0d35406566d18ab2f4af29f
John Walker <jw4610@ic.ac.uk>
2012-11-29 18:14:38 +0000

    merged with gios

--- CBRinit_john.m
+++ CBRinit_john.m
@@ -4,6 +4,14 @@ cluster_cell_array = create_clusters(exam_to_au(x), y);
 %Rank how similar each cluster is to each other 
 sim_rankings = inital_ranking(cluster_cell_array);
 
+for i = 1:length(sim_rankings)
+    cbr_system.cluster{i} = [];
+    cluster               = cluster_cell_array{i};
+    rankings              = sim_rankings{i};
+    for j = 1:length(cluster);
+        cbr_system.cluster{i} = insert_heap(cbr_system.cluster{i}, cluster(j), rankings(j)); 
+    end
+end
 
 end
 
